00e393763467ee26053c41fd030f749729c8d248
Jason <jasonwaters77@msn.com>
2025-05-23 12:55:06 -0600

    Add .gitignore rules

---
+++ addons/zOldAddons/React/react_WAR.lua
@@ -0,0 +1,39 @@
+return {
+    ["Asb"]={
+        ["Shah Mat"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    }, 
+	["Asb"]={
+        ["Tabbiyaa Gambit"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    }, 
+	
+    ["Rukh"]={
+        ["Shah Mat"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    }, 
+    ["Shah"]={
+        ["Shah Mat"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    }, 
+    ["Wazir"]={
+        ["Shah Mat"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    }, 
+    ["Pil"]={
+        ["Shah Mat"]={
+            ["complete_reaction"]="facemob", 
+            ["ready_reaction"]="turnaround"
+        }
+    }
+}
---
+++ addons/zOldAddons/Synthchain/synthchain_Dawnfergie.lua
@@ -0,0 +1,8 @@
+return {
+    [1]="Liquefaction", 
+    [2]="Scission", 
+    [3]="Liquefaction", 
+    [4]="Impaction", 
+    [5]="Fragmentation", 
+    [6]="Light"
+}
